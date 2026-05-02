d4115fc09d15d08ac796537ab9c6849185bbb29f
sshivanshg <sshivanshg@gmail.com>
2026-05-02 10:37:40 +0530

    subnet fix

--- terraform/data.tf
+++ terraform/data.tf
@@ -4,9 +4,16 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Default-VPC subnets that are the primary subnet per AZ (one per AZ). Using all subnets
+# sorted by id can pick two subnets in the same AZ and breaks ALB: "cannot be attached to
+# multiple subnets in the same Availability Zone".
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
   }
 }
